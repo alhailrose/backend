975be8a49c058d9dca2f699603dc986f02f898b2
alhailrose <sfqbgs77@gmail.com>
2024-05-04 22:10:11 +0700

    asik

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Service Backend is running");
+    res.send("Service Backend is running asik banget");
 });
 
 app.listen(PORT, () => {
